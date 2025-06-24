ef90565ea199056a49182e968333d918e1b3af3f
Gabriela Kotseva <gkotseva98@gmail.com>
2025-06-24 14:09:26 +0300

    New components created

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,9 @@
+export function Button() {
+  return (
+    <div>
+      <button className="px-8 py-2 rounded-4xl bg-blue-500 text-white hover:bg-blue-300 hover:text-black uppercase tracking-widest transition-colors duration-400 ease-in-out">
+        Submit
+      </button>
+    </div>
+  );
+}
