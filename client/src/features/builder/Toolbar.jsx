5cfad21eaa280794969bcd5c6558bb3cd106bb3f
Gabriela Kotseva <gkotseva98@gmail.com>
2025-07-15 17:12:22 +0300

    Made the preview and export buttons work as expected

--- client/src/features/builder/Toolbar.jsx
+++ client/src/features/builder/Toolbar.jsx
@@ -1,10 +1,56 @@
 import "./Toolbar.modules.css";
 
-export const Toolbar = () => {
+import html2pdf from "html2pdf.js";
+
+export const Toolbar = ({ cvRef, setIsPreviewMode }) => {
+  const generatePdfDownload = () => {
+    const element = cvRef.current;
+
+    const opt = {
+      filename: "CV.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+    };
+
+    html2pdf().set(opt).from(element).save();
+  };
+
+  const generatePdfPreview = () => {
+    const element = cvRef.current;
+
+    setIsPreviewMode(true);
+
+    setTimeout(() => {
+      const opt = {
+        filename: "CV.pdf",
+        image: { type: "jpeg", quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+      };
+
+      html2pdf()
+        .set(opt)
+        .from(element)
+        .outputPdf("blob")
+        .then((blob) => {
+          const url = URL.createObjectURL(blob);
+          window.open(url);
+        })
+        .finally(() => {
+          setIsPreviewMode(false);
+        });
+    }, 100);
+  };
+
   return (
     <div className="toolbar">
-      <button className="orange-button">Preview</button>
-      <button className="blue-button">Export</button>
+      <button className="orange-button" onClick={generatePdfPreview}>
+        Preview
+      </button>
+      <button className="blue-button" onClick={generatePdfDownload}>
+        Export
+      </button>
       <button className="green-button">Save</button>
     </div>
   );
