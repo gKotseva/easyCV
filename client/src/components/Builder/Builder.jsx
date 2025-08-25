391b5bf49212ff8f0a1bc9e8236ddb77f0951f95
Gabriela Kotseva <gkotseva98@gmail.com>
2025-08-25 15:44:05 +0300

    New component created

---
+++ client/src/components/Builder/Builder.jsx
@@ -0,0 +1,48 @@
+import "./Builder.modules.css";
+
+import { useCV } from "../../contexts/CV";
+import { Education } from "../Sections/Education";
+import { Summary } from "../Sections/Summary";
+import { TechnicalSkills } from "../Sections/TechnicalSkills";
+import { Work } from "../Sections/Work";
+import { SoftSkills } from "../Sections/SoftSkills";
+import { ContactInformation } from "../Sections/ContactInformation";
+import { Introduction } from "../Sections/Introduction";
+import { CVControllers } from "../CVControllers";
+
+export const Builder = () => {
+  const { columns, theme, styling, sections, saveCV } = useCV();
+
+  const components = {
+    introduction: Introduction,
+    contact_information: ContactInformation,
+    summary: Summary,
+    soft_skills: SoftSkills,
+    technical_skills: TechnicalSkills,
+    work_experience: Work,
+    education: Education,
+  };
+
+  return (
+    <div className="builder-container">
+      <div className="side-container"></div>
+      <div className="body-container">
+        <div className="action-buttons">
+          <CVControllers saveHandler={saveCV} />
+        </div>
+        <div className="cv-container">
+          <div className="cv">
+            {sections.map((section) => {
+              const Component = components[section.section_label];
+              return Component ? (
+                <div key={section.section_label}>
+                  <Component theme={theme} section={section} />
+                </div>
+              ) : null;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
