16fd8e2da086520cc986cad597a986e788f28790
Gabriela Kotseva <gkotseva98@gmail.com>
2025-08-27 19:35:30 +0300

    updated the context for more flexibility

--- client/src/contexts/CV.jsx
+++ client/src/contexts/CV.jsx
@@ -1,42 +1,68 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { getSections, save, update } from "../handlers/cv";
+import { createContext, useContext, useState, useEffect } from "react";
+import { getDocuments, getSections, save, update } from "../handlers/cv";
+import { useNavigate } from "react-router-dom";
 import { useNotification } from "./Notification";
 
 const CVContext = createContext();
 
 export function CVProvider({ children }) {
+  const navigate = useNavigate();
   const { showNotification } = useNotification();
-
-  const [id, setId] = useState(null);
+  const [cvId, setCvId] = useState(null);
+  const [userId, setUserId] = useState(1);
+  const [sections, setSections] = useState([]);
   const [name, setName] = useState("");
   const [columns, setColumns] = useState(1);
   const [theme, setTheme] = useState("theme1");
   const [styling, setStyling] = useState({});
-  const [sections, setSections] = useState([]);
   const [hasChanges, setHasChanges] = useState(false);
 
   useEffect(() => {
-    const fetchSections = async () => {
-      const data = await getSections();
-      setSections(data);
+    const loadDefaultSections = async () => {
+      const defaultSections = await getSections();
+      setSections(defaultSections);
     };
-    fetchSections();
+    loadDefaultSections();
   }, []);
 
-  const updateField = async (sectionId, field, value) => {
+  const loadCV = async (cvId, cvData) => {
+    if (!cvId) {
+      const response = await getSections();
+      setSections(response);
+      navigate(`/builder`);
+    } else {
+      const mergedSections = (await getSections()).map((section) => ({
+        ...section,
+        values: cvData.data[section.section_id] || {},
+      }));
+      setSections(mergedSections);
+      setCvId(cvId);
+      setName(cvData.title);
+      setTheme(cvData.theme);
+      setColumns(cvData.columns);
+      setStyling(cvData.styling || {});
+      navigate(`/builder/${cvId}`);
+    }
+  };
+
+  const updateField = (sectionId, field, value, idx = null) => {
     setHasChanges(true);
+
     setSections((prevSections) =>
-      prevSections.map((section) =>
-        section.section_id === sectionId
-          ? {
-              ...section,
-              values: {
-                ...(section.values || {}),
-                [field]: value,
-              },
+      prevSections.map((s) => {
+        if (s.section_id === sectionId) {
+          if (s.multiple === 0) {
+            return { ...s, values: { ...(s.values || {}), [field]: value } };
+          } else if (s.multiple === 1) {
+            const updatedValues = [...(s.values || [])];
+            if (idx !== null && updatedValues[idx]) {
+              updatedValues[idx] = { ...updatedValues[idx], [field]: value };
             }
-          : section
-      )
+            return { ...s, values: updatedValues };
+          }
+        }
+        return s;
+      })
     );
   };
 
@@ -48,13 +74,11 @@ export function CVProvider({ children }) {
 
     setHasChanges(false);
     let response;
-    if (id !== null) {
-      console.log(name);
-      response = await update(name, columns, theme, styling, sections, id);
+    if (cvId !== null) {
+      response = await update(name, columns, theme, styling, sections, cvId);
     } else {
-      console.log(name);
-      response = await save(name, columns, theme, styling, sections, id);
-      setId(response.cvId);
+      response = await save(name, columns, theme, styling, sections, userId);
+      setCvId(response.cvId);
     }
 
     showNotification("CV Saved", "success");
@@ -78,14 +102,15 @@ export function CVProvider({ children }) {
   return (
     <CVContext.Provider
       value={{
+        loadCV,
+        updateField,
+        saveCV,
+        setName,
+        sections,
+        name,
         columns,
         theme,
         styling,
-        sections,
-        updateField,
-        saveCV,
-        name,
-        setName,
       }}
     >
       {children}
