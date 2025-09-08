044f33df05df5b9a30516aebb2e8109ee8710640
Gabriela Kotseva <gkotseva98@gmail.com>
2025-09-08 18:27:05 +0300

    Styling updates for colors to use variables

--- client/src/pages/Builder.jsx
+++ client/src/pages/Builder.jsx
@@ -1,13 +1,12 @@
+import "./Builder.modules.css";
+
 import { BuilderNav } from "../components/Builder/BuilderNav";
 import { BuilderSide } from "../components/Builder/BuilderSide";
 import { CVEditor } from "../components/Builder/CVEditor";
 
 export const Builder = () => {
   return (
-    <div
-      className="builder-page-container"
-      style={{ background: "#1f252d", color: "white" }}
-    >
+    <div className="builder-page-container">
       <BuilderNav />
       <div
         className="builder-body"
