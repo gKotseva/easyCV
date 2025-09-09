ee2d1a61a176e245b5b934e320e201110ea94dd6
Gabriela Kotseva <gkotseva98@gmail.com>
2025-09-09 15:31:25 +0300

    Updated handler requests

--- client/src/handlers/cv.js
+++ client/src/handlers/cv.js
@@ -1,48 +1,26 @@
 import * as request from "../lib/request";
 
-export const getSections = async () => {
-  try {
-    const response = await request.get("/api/cv/sections");
-    return response;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const save = async (name, columns, theme, styling, sections) => {
-  try {
-    const response = await request.post("/api/cv/save", {
-      name,
-      columns,
-      theme,
-      styling,
-      sections,
-    });
-    return response;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const update = async (name, columns, theme, styling, sections, cvId) => {
-  try {
-    const response = await request.put("/api/cv/update", {
-      name,
-      columns,
-      theme,
-      styling,
-      sections,
-      cvId,
-    });
-    return response;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const getDocuments = async (userId) => {
+export const getAllDocuments = async (userId) => {
   try {
     const response = await request.get(`/api/cv/documents?userId=${userId}`);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteDocument = async (cvId) => {
+  try {
+    const response = await request.remove(`/api/cv/delete`, { cvId });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const renameDocument = async (cvId, title) => {
+  try {
+    const response = await request.put(`/api/cv/rename`, { cvId, title });
     return response;
   } catch (error) {
     throw error;
