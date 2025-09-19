d01ee50e34b030ba3e29dc408ad206773e3ea530
Gabriela Kotseva <gkotseva98@gmail.com>
2025-09-19 17:45:52 +0300

    Updated the inner section and section controllers to use the functions in the cv context

--- client/src/components/Builder/SectionControllers.jsx
+++ client/src/components/Builder/SectionControllers.jsx
@@ -1,12 +1,18 @@
 import { IoIosReorder } from "react-icons/io";
 import { GoTrash } from "react-icons/go";
+import { useCV } from "../../context/CV";
 
-export const SectionControllers = ({ children }) => {
+export const SectionControllers = ({ children, section }) => {
+  const { removeSection } = useCV();
+
   return (
     <div className="section-wrapper">
       <div className="controllers">
-        <IoIosReorder />
-        <GoTrash color="red" />
+        <IoIosReorder onClick={() => console.log("reordering")} />
+        <GoTrash
+          color="red"
+          onClick={() => removeSection(section.section_id)}
+        />
       </div>
       <div className="section-content">{children}</div>
     </div>
--- client/src/components/Builder/SectionInnerControllers.jsx
+++ client/src/components/Builder/SectionInnerControllers.jsx
@@ -1,11 +1,17 @@
 import { IoIosAddCircle } from "react-icons/io";
 import { MdOutlineRemoveCircle } from "react-icons/md";
+import { useCV } from "../../context/CV";
 
-export const SectionInnerControllers = () => {
+export const SectionInnerControllers = ({ section, removeId }) => {
+  const { addSectionItem, removeSectionItem } = useCV();
+
   return (
     <div className="inner-controllers">
-      <IoIosAddCircle color="green" />
-      <MdOutlineRemoveCircle color="red" />
+      <IoIosAddCircle color="green" onClick={() => addSectionItem(section)} />
+      <MdOutlineRemoveCircle
+        color="red"
+        onClick={() => removeSectionItem(section, removeId)}
+      />
     </div>
   );
 };
