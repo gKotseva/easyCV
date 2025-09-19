41fef55f2517537066ba432313f69245d339381c
Gabriela Kotseva <gkotseva98@gmail.com>
2025-09-19 17:45:19 +0300

    Added functions for removing sections, adding section items and removing section items

--- client/src/context/cv.jsx
+++ client/src/context/cv.jsx
@@ -1,36 +1,102 @@
 import { createContext, useContext, useState } from "react";
+import { sections } from "../testdata";
+import { formatCVData } from "../utils/formatCVData";
 
 export const CVContext = createContext();
 
 export function CVProvider({ children }) {
-  const [cv, setCv] = useState({
+  const [cv, setCv] = useState(() => ({
     cv_id: "9",
     user_id: "1",
     title: "Gabi",
-    data: {
-      personalInformation: {
-        fullName: "Gabriela Kotseva",
-      },
-    },
-  });
+    data: formatCVData(sections),
+  }));
 
-  const editCV = (section, field, newValue) => {
+  const editCV = (section, field, newValue, idx = 0) => {
+    setCv((prev) => {
+      const newData = prev.data.map((d) => {
+        const key = Object.keys(d)[0];
+
+        if (Number(key) === section.section_id) {
+          const value = d[key];
+
+          if (section.section_multiple) {
+            const updatedValue = value.map((item, i) =>
+              i === idx ? { ...item, [field]: newValue } : item
+            );
+            return { [key]: updatedValue };
+          } else {
+            return { [key]: { ...value, [field]: newValue } };
+          }
+        }
+
+        return d;
+      });
+
+      return { ...prev, data: newData };
+    });
+  };
+
+  const removeSection = (sectionId) => {
     setCv((prev) => ({
       ...prev,
-      data: {
-        ...prev.data,
-        [section.section_label]: {
-          ...(prev.data?.[section.section_label] || {}),
-          [field]: newValue,
-        },
-      },
+      data: prev.data.filter((obj) => {
+        const key = Object.keys(obj)[0];
+        return Number(key) !== sectionId;
+      }),
     }));
   };
 
-  console.log(cv);
+  const addSectionItem = (section) => {
+    setCv((prev) => {
+      const newData = prev.data.map((d) => {
+        const key = Object.keys(d)[0];
+
+        if (Number(key) === section.section_id) {
+          const updatedArray = [
+            ...d[key],
+            Object.fromEntries(section.section_fields.map((f) => [f, ""])),
+          ];
+
+          return { [key]: updatedArray };
+        }
+
+        return d;
+      });
+
+      return { ...prev, data: newData };
+    });
+  };
+
+  const removeSectionItem = (section, idxToRemove) => {
+    setCv((prev) => {
+      const newData = prev.data.map((d) => {
+        const key = Object.keys(d)[0];
+
+        if (Number(key) === section.section_id && section.section_multiple) {
+          const updatedArray = d[key].filter((_, idx) => idx !== idxToRemove);
+          return { [key]: updatedArray };
+        }
+
+        return d;
+      });
+
+      return { ...prev, data: newData };
+    });
+  };
 
   return (
-    <CVContext.Provider value={{ cv, setCv, editCV }}>
+    <CVContext.Provider
+      value={{
+        cv,
+        sections,
+        setCv,
+        editCV,
+        removeSection,
+        addSectionItem,
+        removeSectionItem,
+      }}
+    >
       {children}
     </CVContext.Provider>
   );
