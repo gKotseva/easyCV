4a1a2be8777c9a554c0f1d3457612562a58e9a4f
Gabriela Kotseva <gkotseva98@gmail.com>
2025-10-08 17:06:41 +0300

    Added internal id's for each entry on a section

--- client/src/utils/formatCVData.js
+++ client/src/utils/formatCVData.js
@@ -1,15 +1,18 @@
 export const formatCVData = (sections) => {
   const data = [];
 
-  sections.map((section) => {
+  sections.forEach((section) => {
     const obj = section.section_fields.reduce(
       (a, v) => ({ ...a, [v]: "" }),
       {}
     );
 
-    section.section_multiple
-      ? data.push({ [section.section_id]: [obj] })
-      : data.push({ [section.section_id]: obj });
+    if (section.section_multiple) {
+      const objWithId = { ...obj, _internalId: Date.now() };
+      data.push({ [section.section_id]: [objWithId] });
+    } else {
+      data.push({ [section.section_id]: obj });
+    }
   });
 
   return data;
